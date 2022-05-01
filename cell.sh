08319cd268402082fe08c91ad7e0359b1278ab77
xuegangjie <631126217@qq.com>
2022-05-02 05:25:59 +0800

    [feat] add alias

--- cell.sh
+++ cell.sh
@@ -6,5 +6,4 @@ echo "CREATING A CELL"
 vtctlclient AddCellInfo \
   -root /vitess/cell1 \
   -server_address $TOPO_SERVER \
-  -server "localhost:15999" \
   cell1
--- env.sh
+++ env.sh
@@ -3,4 +3,4 @@ TOPO_SERVER=localhost:2379
 
 TOPOLOGY_FLAGS="--topo_implementation etcd2 --topo_global_server_address $TOPO_SERVER --topo_global_root /vitess/global"
 
-
+alias vtctlclient="command vtctlclient --server localhost:15999 --log_dir ${VTDATAROOT}/tmp --alsologtostderr"
