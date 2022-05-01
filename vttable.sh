6bfe977acc788206ebdd21cad7677dff5ef47a68
xuegangjie <631126217@qq.com>
2022-05-02 06:45:06 +0800

    [feat] primary

--- vttable.sh
+++ vttable.sh
@@ -6,7 +6,6 @@ echo "Start Vttablet"
 vttablet -topo_implementation=etcd2 -topo_global_server_address=$TOPO_SERVER -topo_global_root=/vitess/global \
   -log_dir=${VTDATAROOT}/tmp \
   -tablet-path=cell1-100 \
-  -tablet_hostname=node02 \
   -init_keyspace=user_bill \
   -init_shard=0 \
   -init_tablet_type=replica \
