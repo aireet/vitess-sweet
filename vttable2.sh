bc6633bb3421bab05da44623b83ef34a7ef363d3
xuegangjie <631126217@qq.com>
2022-05-03 05:49:19 +0800

    [feat] vttable 2

--- vttable2.sh
+++ vttable2.sh
@@ -6,19 +6,25 @@ echo "Start Vttablet"
 vttablet -topo_implementation=etcd2 -topo_global_server_address=$TOPO_SERVER -topo_global_root=/vitess/global \
   -log_dir=${VTDATAROOT}/tmp \
   -tablet-path=cell1-200 \
-  -init_keyspace=user_bill \
-  -init_shard=1 \
   -db_host=127.0.0.1 \
-  -db_port=33100 \
-  -db_app_user=echo \
-  -db_app_password=echo2022 \
+  -db_port=3306 \
+  -db_dba_user=root \
+  -db_dba_password=echo2022!~Aa \
+  -db_app_user=root \
+  -db_app_password=echo2022!~Aa \
+  -db_allprivs_user=root \
+  -db_allprivs_password=echo2022!~Aa \
+  -db_filtered_user=root \
+  -db_filtered_password=echo2022!~Aa \
+  -init_keyspace=user_bill \
+  -db-config-erepl-dbname=vt_user_bill_1 \
+  -init_shard=1 \
   -init_tablet_type=replica \
-  -port=15101 \
-  -grpc_port=16101 \
+  -port=15100 \
+  -grpc_port=16100 \
   -service_map 'grpc-queryservice,grpc-tabletmanager' \
   -enable_semi_sync=false \
   -enable_replication_reporter=false \
-  -restore_from_backup=false \
   -queryserver-config-pool-size=16 \
   -queryserver-config-transaction-cap=300 \
   -queryserver-config-stream-pool-size=16 \
