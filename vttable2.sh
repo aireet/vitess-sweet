a55550275db32537ddae38b79a35b62c241cee44
xuegangjie <631126217@qq.com>
2022-05-03 05:51:37 +0800

    [feat] vttable db2

--- vttable2.sh
+++ vttable2.sh
@@ -20,8 +20,8 @@ vttablet -topo_implementation=etcd2 -topo_global_server_address=$TOPO_SERVER -to
   -db-config-erepl-dbname=vt_user_bill_1 \
   -init_shard=1 \
   -init_tablet_type=replica \
-  -port=15100 \
-  -grpc_port=16100 \
+  -port=15101 \
+  -grpc_port=16101 \
   -service_map 'grpc-queryservice,grpc-tabletmanager' \
   -enable_semi_sync=false \
   -enable_replication_reporter=false \
