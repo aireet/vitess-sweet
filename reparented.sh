db46238414a922ba4fc3e4f0dc35985b0a0220cd
xuegangjie <631126217@qq.com>
2022-05-04 04:57:02 +0800

    [feat] add shard

--- reparented.sh
+++ reparented.sh
@@ -7,6 +7,9 @@ source ./env.sh
 vtctl -topo_implementation=etcd2 -topo_global_server_address=localhost:2379 -topo_global_root=/vitess/global TabletExternallyReparented cell1-0000000100 -disable_active_reparents
 
 
+vtctlclient TabletExternallyReparented --force user_bill/-80 cell1-100
+
+
 vtctl -topo_implementation=etcd2 -topo_global_server_address=localhost:2379 -topo_global_root=/vitess/global DeleteTablet -allow_primary cell1-0000000200
 
 vtctlclient InitShardPrimary -force user_bill/0 cell1-100
