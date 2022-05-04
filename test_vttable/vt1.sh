db766f708a872be85d8811e6b48f4b3019b7d135
xuegangjie <631126217@qq.com>
2022-05-04 12:08:30 +0800

    [feat] vttable

---
+++ test_vttable/vt1.sh
@@ -0,0 +1,33 @@
+
+source ../env.sh
+
+# vttablet
+echo "Start Vttablet"
+vttablet -topo_implementation=etcd2 -topo_global_server_address=$TOPO_SERVER -topo_global_root=/vitess/global \
+  -log_dir=${VTDATAROOT}/tmp \
+  -tablet-path=cell1-1 \
+  -vtctld_addr=127.0.0.1:15000 \
+  -health_check_interval=5s \
+  -db_host=127.0.0.1 \
+  -db_port=3306 \
+  -db_dba_user=root \
+  -db_dba_password=echo2022!~Aa \
+  -db_app_user=root \
+  -db_app_password=echo2022!~Aa \
+  -db_allprivs_user=root \
+  -db_allprivs_password=echo2022!~Aa \
+  -db_filtered_user=root \
+  -db_filtered_password=echo2022!~Aa \
+  -init_keyspace=TestA \
+  -init_shard=-80 \
+  -init_tablet_type=replica \
+  -port=15101 \
+  -grpc_port=16101 \
+  -service_map 'grpc-queryservice,grpc-tabletmanager,grpc-updatestream' \
+  -enable_semi_sync=false \
+  -enable_replication_reporter=false \
+  -queryserver-config-pool-size=16 \
+  -queryserver-config-transaction-cap=300 \
+  -queryserver-config-stream-pool-size=16 \
+ -pid_file $VTDATAROOT/tmp/vttablet.pid \
+ > $VTDATAROOT/tmp/vttablet.out 2>&1 &
