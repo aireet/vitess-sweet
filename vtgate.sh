777dc184b02f142929511844625258adc3a23a49
xuegangjie <631126217@qq.com>
2022-05-05 07:54:57 +0800

    [feat] tracing

--- vtgate.sh
+++ vtgate.sh
@@ -12,6 +12,7 @@ vtgate -topo_implementation=etcd2 -topo_global_server_address=$TOPO_SERVER -topo
   -mysql_auth_server_impl=static \
   -mysql_auth_server_static_file=/app/vitess/vitess-sweet/mysql.json \
   -grpc_port=15991 \
+  -tracer opentracing-jaeger -jaeger-agent-host 127.0.0.1:6831 -tracing-sampling-rate 0.0 \
   -service_map='grpc-vtgateservice' \
   -vschema_ddl_authorized_users='%' \
   -pid_file $VTDATAROOT/vtgate/vtgate.pid \
