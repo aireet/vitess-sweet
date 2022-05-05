b523df1fd91804e7af8cde6b83d41a7ab7fb6a3d
xuegangjie <631126217@qq.com>
2022-05-05 14:22:24 +0800

    [feat] rename

--- reparented.sh
+++ reparented.sh
@@ -7,11 +7,11 @@ source ./env.sh
 vtctl -topo_implementation=etcd2 -topo_global_server_address=localhost:2379 -topo_global_root=/vitess/global TabletExternallyReparented cell1-0000000100 -disable_active_reparents
 
 
-vtctlclient TabletExternallyReparented cell1-1
-vtctlclient TabletExternallyReparented cell1-2
+vtctlclient TabletExternallyReparented cell1-100
+vtctlclient TabletExternallyReparented cell1-200
 vtctlclient TabletExternallyReparented cell1-3
 
 
-vtctl -topo_implementation=etcd2 -topo_global_server_address=localhost:2379 -topo_global_root=/vitess/global DeleteTablet -allow_primary cell1-0000000200
+vtctl -topo_implementation=etcd2 -topo_global_server_address=localhost:2379 -topo_global_root=/vitess/global DeleteTablet -allow_primary cell1-0000000100
 
 vtctlclient InitShardPrimary -force user_bill/0 cell1-100
